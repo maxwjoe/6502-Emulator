a735fe5d586f71a54926ab3b5796b36ca59a22c7
Joel <joel.r.maxwell@gmail.com>
2022-12-08 12:57:01 +1100

    Define STA, STX, STY OpCodes

--- include/Opcodes.h
+++ include/Opcodes.h
@@ -1,18 +1,23 @@
 #ifndef OPCODES_H
 #define OPCODES_H
 
-// Note : Defines Code : Description (Addr Mode, CPU Cycles) : Flags (C, T) => C = Complete,  T = Tested
+// --- File Format ---
+
+// #define [OP_NAME] 0x[CODE] // Description (Addressing Mode, CPU Cycles) : FLAGS
+// FLAGS : C = Complete, T = Tested , FLAG + * = Has Active TODO Item
+
+// --- Opcode Definitions ---
 
 // LDA : Load Accumulator
 
-#define LDA_IM 0xA9  // Load Accumulator (Immediate Mode, 2) : C
-#define LDA_ZP 0xA5  // Load Accumulator (Zero Page Mode, 3) : C
-#define LDA_ZPX 0xB5 // Load Accumulator (Zero Page X Mode, 4) : C
-#define LDA_AB 0xAD  // Load Accumulator (Absolute Mode, 4) : C
-#define LDA_ABX 0xBD // Load Accumulator (Absolute X Mode, 4 + 1 if page cross) : C*
-#define LDA_ABY 0xB9 // Load Accumulator (Absolute Y Mode, 4 + 1 if page cross) : C*
-#define LDA_INX 0xA1 // Load Accumulator (Indirect X, 6) : C
-#define LDA_INY 0xB1 // Load Accumulator (Indirect Y, 5 + 1 if page cross) : C
+#define LDA_IM 0xA9  // Load Accumulator (Immediate Mode, 2) : C, T
+#define LDA_ZP 0xA5  // Load Accumulator (Zero Page Mode, 3) : C, T
+#define LDA_ZPX 0xB5 // Load Accumulator (Zero Page X Mode, 4) : C, T
+#define LDA_AB 0xAD  // Load Accumulator (Absolute Mode, 4) : C, T
+#define LDA_ABX 0xBD // Load Accumulator (Absolute X Mode, 4 + 1 if page cross) : C*, T*
+#define LDA_ABY 0xB9 // Load Accumulator (Absolute Y Mode, 4 + 1 if page cross) : C*, T*
+#define LDA_INX 0xA1 // Load Accumulator (Indirect X, 6) : C*, T*
+#define LDA_INY 0xB1 // Load Accumulator (Indirect Y, 5 + 1 if page cross) : C*, T*
 
 // LDX : Load X Register
 
@@ -29,6 +34,28 @@
 #define LDY_ZPX 0xB4 // Load Y Register (Zero Page X Mode, 4) : C
 #define LDY_AB 0xAC  // Load Y Register (Absolute Mode, 4) : C
 #define LDY_ABX 0xBC // Load Y Register (Absolute X Mode, 4 + 1 if page cross) : C*
+
+// STA : Store Accumulator
+
+#define STA_ZP 0x85  // Store Accumulator (Zero Page Mode, 3) :
+#define STA_ZPX 0x95 // Store Accumulator (Zero Page X Mode, 4) :
+#define STA_AB 0x8D  // Store Accumulator (Absolute Mode, 4) :
+#define STA_ABX 0x9D // Store Accumulator (Absolute X Mode, 5) :
+#define STA_ABY 0x99 // Store Accumulator (Absolute Y Mode, 5) :
+#define STA_INX 0x81 // Store Accumulator (Indirect X Mode, 6) :
+#define STA_INY 0x91 // Store Accumulator (Indirect Y Mode, 6) :
+
+// STX : Store X Register
+
+#define STX_ZP 0x86  // Store X Register (Zero Page Mode, 3) :
+#define STX_ZPY 0x96 // Store X Register (Zero Page Y Mode, 4) :
+#define STX_AB 0x8E  // Store X Register (Absolute Mode, 4) :
+
+// STY : Store Y Register
+
+#define STY_ZP 0x84  // Store Y Register (Zero Page Mode, 3) :
+#define STY_ZPX 0x94 // Store Y Register (Zero Page Y Mode, 4) :
+#define STY_AB 0x8C  // Store Y Register (Absolute Mode, 4) :
 
 // JSR : Jump to Subroutine
 
